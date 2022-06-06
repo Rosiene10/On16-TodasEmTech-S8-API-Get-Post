917a8225f28f238dac4443361d57ab60e6bab581
rosienecorreia <rosienecorreia30@gmail.com>
2022-06-06 08:44:29 -0300

    feat:implemetei uma rota get para trazer todos os filmes

--- tarefa-de-casa/src/server.js
+++ tarefa-de-casa/src/server.js
@@ -3,6 +3,8 @@ const { request } = require('express')
 const express=require('express')
 const app=express()
 
+const ghiblifilmsJson=require("./models/ghiblifilms.json")
+
 app.listen(3000,()=>{
     console.log("servidor rodando")
 })
@@ -11,4 +13,8 @@ app.get("/",(request,response)=>{
     response.status(200).json([{
         "mensagem":"tudo certo"
     }])
+})
+
+app.get("/ghiblifilms",(resquest,response)=>{
+    response.status(200).send(ghiblifilmsJson)
 })
